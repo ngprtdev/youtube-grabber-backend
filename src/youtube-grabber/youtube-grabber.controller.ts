8027bb9fd2f671ad5a2de906b704a3f3b6fe63e8
ngprt <nicolas@tokee.ai>
2024-09-05 14:00:56 +0200

    allow direct download

--- src/youtube-grabber/youtube-grabber.controller.ts
+++ src/youtube-grabber/youtube-grabber.controller.ts
@@ -1,8 +1,43 @@
-import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  BadRequestException,
+  Res,
+  StreamableFile,
+} from '@nestjs/common';
+import { Response } from 'express';
 import axios from 'axios';
+import { Readable } from 'stream';
 
 @Controller('youtube-thumbnail')
 export class YoutubeController {
+  @Post('download')
+  async downloadThumbnail(
+    @Body() body: { url: string },
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<StreamableFile> {
+    const { url } = body;
+
+    try {
+      if (!url || !url.startsWith('https://img.youtube.com/vi/')) {
+        throw new BadRequestException('Invalid or empty thumbnail URL');
+      }
+
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+      res.set({
+        'Content-Disposition': 'attachment; filename=thumbnail.jpg',
+        'Content-Type': 'image/jpeg',
+      });
+
+      const stream = Readable.from(response.data);
+      return new StreamableFile(stream);
+    } catch (error) {
+      throw new BadRequestException('Failed to download the image.');
+    }
+  }
+
   @Post()
   async getThumbnail(@Body() body: { url: string }) {
     const videoId = this.extractVideoId(body.url);
